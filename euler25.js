4a875e93bba03a33c9c4807d7bcab1e3f3a8d5b1
Francisco Rafart <francisco.rafart@gmail.com>
2018-03-29 07:17:21 -0400

    solution

--- euler25.js
+++ euler25.js
@@ -1,4 +1,15 @@
-console.log(sumStr('3039','234'));
+let fib=['1','1']
+let i = 0;
+while(true){
+  let fibo = sumStr(fib[i],fib[i+1]);
+  fib.push(fibo)
+
+  if (fib[i+2].length>=1000){
+    console.log('F'+(i+3)+' = '+fibo) // i+3 is the index starting from 1
+    break
+  }
+  i++
+}
 
 //function to sum strings numbers
 function sumStr(str, str2){
@@ -13,8 +24,9 @@ function sumStr(str, str2){
   }
 
   for (let idxa = str.length-1, idxb= str2.length-1; idxa>=0 || idxb>=0; idxa--, idxb--){
-    let digit1, digit2 = 0;
-    //control
+    let digit1 = 0;
+    let digit2 = 0;
+
     if (str.charAt(idxa)){
       digit1 = Number(str.charAt(idxa));
     }
@@ -25,20 +37,25 @@ function sumStr(str, str2){
     localRes = digit1 + digit2 + extra;
 
     if(localRes >= 10){
-      res+=Number(localRes-10)
+      res += String(localRes-10)
       extra = 1;
     } else {
       res += String(localRes);
       extra = 0;
     }
   }
+  //add '1' if there's extra remaining on last round
+  if(extra !== 0){
+    res+='1'
+  }
+
   res = reverse(res)
   return res
 }
 
 function reverse(str){
   let res = ''
-  for (let i=str.length;i>=0;i--){
+  for (let i=str.length-1;i>=0;i--){
     res+=str.charAt(i)
   }
   return res
