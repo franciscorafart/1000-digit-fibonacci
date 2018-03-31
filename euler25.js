5ff718bd1d0628259f5c25fc23e8ffb8b9c51c36
Francisco Rafart <francisco.rafart@gmail.com>
2018-03-30 22:20:34 -0400

    format

--- euler25.js
+++ euler25.js
@@ -1,27 +1,22 @@
-let fib=['1','1']
+let fib=['1','1'];
 let i = 0;
 while(true){
   let fibo = sumStr(fib[i],fib[i+1]);
-  fib.push(fibo)
+
+  fib.push(fibo);
 
   if (fib[i+2].length>=1000){
     console.log('F'+(i+3)+' = '+fibo) // i+3 is the index starting from 1
-    break
+    break;
   }
-  i++
+  i++;
 }
 
-//function to sum strings numbers
+//function to sum numbers as strings
 function sumStr(str, str2){
   let extra = 0;
   let length;
-  let res = ''
-
-  if (str.length > str2.length){
-    length = str.length
-  } else {
-    length = str2.length
-  }
+  let res = '';
 
   for (let idxa = str.length-1, idxb= str2.length-1; idxa>=0 || idxb>=0; idxa--, idxb--){
     let digit1 = 0;
@@ -37,7 +32,7 @@ function sumStr(str, str2){
     localRes = digit1 + digit2 + extra;
 
     if(localRes >= 10){
-      res += String(localRes-10)
+      res += String(localRes-10);
       extra = 1;
     } else {
       res += String(localRes);
@@ -46,17 +41,17 @@ function sumStr(str, str2){
   }
   //add '1' if there's extra remaining on last round
   if(extra !== 0){
-    res+='1'
+    res+='1';
   }
 
-  res = reverse(res)
-  return res
+  res = reverse(res);
+  return res;
 }
 
 function reverse(str){
-  let res = ''
-  for (let i=str.length-1;i>=0;i--){
-    res+=str.charAt(i)
+  let res = '';
+  for (let i = str.length-1; i >= 0; i--){
+    res+=str.charAt(i);
   }
-  return res
+  return res;
 }
